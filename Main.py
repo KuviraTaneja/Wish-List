b1624abe9edb7996f2b6c77dfded39cd1da73fb4
KuviraTaneja <kuvi.taneja@gmail.com>
2021-09-10 15:31:44 +0530

    Test

---
+++ Main.py
@@ -0,0 +1,38 @@
+from pywebio.input  import *
+from pywebio.output import *
+from functools import partial # call any function
+
+
+def completed(buttonName, task , tasks): # remove the todo
+    if buttonName == 'Click to complete':
+        tasks.remove(task)
+    clear('tasks')
+
+    if tasks:
+        put_table([
+            [todo , put_buttons(['Click to complete'],onclick=partial(completed,task = todo, tasks = tasks))] for todo in tasks
+        ],
+            scope = 'tasks',
+        )
+
+def priority(buttonName,task,tasks):
+    pass
+
+
+def todo():
+    tasks = [] # to append tasks
+    with use_scope('tasks'):
+        while True:
+            todo = input("Hey! Write your To Do : ")
+            tasks.append(todo)
+            clear('tasks')
+            put_table([
+                [todo , put_buttons(['Click to complete'],onclick=partial(completed,task = todo, tasks = tasks)),put_buttons(['Priority'],onclick=partial(priority,task = todo, tasks = tasks))] for todo in tasks
+            ])
+
+
+
+# List comprehension..
+# a = [i for i in range(0,100,3) if i % 2 == 0]
+# print(a)
+todo()
